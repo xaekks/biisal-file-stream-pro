ddafacb72d45b89f20f2cc6047d83bdeae28ebdf
𓆩•𝐊𝐢𝐬𝐬𝐮 💞•𓆪 <kissuXbots@gmail.com>
2024-12-31 04:07:03 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -19,7 +19,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002391526686'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002391526686'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
